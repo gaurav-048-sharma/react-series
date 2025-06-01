ed5ce9a99911960c4e22e62827a0c7f40737b631
rohan <rohansharmas028@gmail.com>
2025-06-01 18:56:22 +0530

    added

---
+++ complex-projects/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+ 
+  return (
+    <>
+<div className="w-full max-h-full flex flex-col justify-center item-center bg-gray-600">
+  <h1 className='text-3xl text-center font-bold text-white'>Welcome to React</h1>
+  <p className='text-center text-white'>This is a simple React application.</p>
+  <p className='text-center text-white'>You can add your components and routes here.</p>
+  <p className='text-center text-white'>For example, you can import and use components like Currency or PassGen.</p>
+  </div>
+    </>
+  )
+}
+
+export default App
